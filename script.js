513138d4888f3f2a09722879dbdc32d3ec38ca99
PCassemiro <pedrobr2909@gmail.com>
2024-10-15 15:44:24 -0300

    inicio/fim

---
+++ script.js
@@ -0,0 +1,7 @@
+function PasseMouse() {
+    var troca= window.document.getElementById('Troca')
+    troca.innerHTML='pedrobr2909@gmail.com';
+}function SaidaMouse() {
+    var troca= window.document.getElementById('Troca')
+    troca.innerHTML='Meu E-Mail';
+}
